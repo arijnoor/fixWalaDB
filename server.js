d0d2bab542fca267103730b1e677a2d77785fc4e
arijnoor <arijnoor023@gmail.com>
2026-07-27 16:45:26 +0500

    done

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ dotenv.config();
 import userRoute from "./routes/userRoute.js";
 const PORT = process.env.PORT;
 const app = express();
+connectDB()
 app.use(cors());
 app.use(express.json());
 app.use("/upload", express.static("upload"));
 
-connectDB();
+
 
 app.use("/api", userRoute);
 
